51dad0bcc17cbb1938c547e09a0df903501d3422
yashmaurya3377 <yashmaurya3377@gmail.com>
2025-08-21 10:13:31 +0530

    auto detect time and save in dataBase

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -19,7 +19,7 @@ const Skill = () => {
   ];
 
   return (
-    <div className="p-6 max-w-4xl mx-auto h-96 py-19">
+    <div className="p-6 max-w-4xl mx-auto h-screen py-19">
       <h1 className="text-3xl font-bold text-center mb-8">My Skills</h1>
       
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
